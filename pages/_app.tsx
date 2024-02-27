abd8ad1b8effeb9134066f0d3b894562853ca02a
Yixing040506 <1805132911@qq.com>
2024-02-27 16:17:45 +0800

    2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
 import { AppProps } from "next/app";
+import prisma from "../lib/prisma";
+import { GetServerSideProps } from 'next';
+import { SessionProvider } from "next-auth/react";
 
-const App = ({ Component, pageProps }: AppProps) => {
+
+function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
-};
+}
 
 export default App;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: String(params?.id),
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return {
+    props: post,
+  };
+};
